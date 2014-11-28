8694223e14aff766b42e5801328464f0ffa495d4
clarkda <dustin.clark@synapse-wireless.com>
2014-11-27 23:33:58 -0600

    semantics

--- spec/command/repo-svn_spec.rb
+++ spec/command/repo-svn_spec.rb
@@ -2,10 +2,8 @@ require File.expand_path('../../spec_helper', __FILE__)
 
 module Pod
   describe Command::RepoSvn do
-    describe 'repo-svn' do
-      it 'registers itself as a command' do
-        Command.parse(%w(repo-svn)).should.be.instance_of Command::RepoSvn
-      end
+    it 'registers itself' do
+      Command.parse(%w(repo-svn)).should.be.instance_of Command::RepoSvn
     end
   end
 end
