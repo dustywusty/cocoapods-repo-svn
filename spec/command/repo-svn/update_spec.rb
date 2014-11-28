6091e22aecdd779bca70fe2412f8f3dd457f23bc
clarkda <dustin.clark@synapse-wireless.com>
2014-11-27 23:35:05 -0600

    base update specs

---
+++ spec/command/repo-svn/update_spec.rb
@@ -0,0 +1,19 @@
+require File.expand_path('../../../spec_helper', __FILE__)
+include CLAide::InformativeError
+
+module Pod
+  describe Command::RepoSvn::Update do
+    it 'registers itself' do
+      Command.parse(%w( repo-svn update )).should.be.instance_of Command::RepoSvn::Update
+    end
+
+    it 'fails without a repository name' do
+      command = Command.parse(%w( repo-svn update ))
+      lambda { command.validate! }.should.raise CLAide::InformativeError
+    end
+
+    it 'updates a repository' do
+      #
+    end
+  end
+end
