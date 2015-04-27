d64e57efe2bcd45fa13934fece147ca34b9d7d0b
Dusty <dustin.clark@synapse-wireless.com>
2015-04-26 22:37:32 -0500

    add svn repo stub, extend temp repo helper, add before do stub

--- spec/command/repo-svn/add_spec.rb
+++ spec/command/repo-svn/add_spec.rb
@@ -1,7 +1,15 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 
+# https://github.com/clarkda/CocoaPods/blob/master/spec/functional/command/repo/add_spec.rb
+
 module Pod
   describe Command::RepoSvn::Add do
+    extend SpecHelper::TemporaryRepos
+
+    before do
+      set_up_test_repo
+    end
+
     it 'returns the proper command class' do
       Command.parse(%w( repo-svn add )).should.be.instance_of Command::RepoSvn::Add
     end
@@ -9,6 +17,10 @@ module Pod
     it 'fails without a repository url or name' do
       command = Command.parse(%w( repo-svn add ))
       lambda { command.validate! }.should.raise CLAide::InformativeError
+    end
+
+    it 'adds a svn spec repo' do
+
     end
   end
 end
